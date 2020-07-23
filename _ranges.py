c9d1cd24a78ec66a3fc92aa66a1a28a441ae512a
Mateus <mscwdesign@gmail.com>
2020-07-23 09:31:49 -0300

    Ranges Python

---
+++ _ranges.py
@@ -0,0 +1,35 @@
+"""
+Ranges
+
+- Precisamos conhecer o loop for para usar os ranges
+- Precisamos conhecer o range para trabalhar melhor com o loops for.
+
+Ranges são utilizados para gerar sequencias numericas, não de forma aleatoria
+mas sim de maneira especificada.
+
+formas gerias:
+
+range(valor_de_parada)
+
+OBS: valor_de_parada não inclusive (inicio padrão 0, e passo de 1 em 1)
+
+# Forma 1
+for num in range(11):
+    print(num)
+
+# Forma 2
+for num in range(1, 11):
+    print(num)
+
+# Exemplo forma 3
+for num in range(1, 10, 2):
+    print(num)
+"""
+
+"""
+
+# Forma 4
+for num in range(10, 0, -3):
+    print(num)
+"""
+
