003e26f67cab7df83996fb630226286c7ccea855
Mateus <mscwdesign@gmail.com>
2020-07-23 09:56:11 -0300

    Dicionarios em Python

---
+++ dicionarios.py
@@ -0,0 +1,21 @@
+"""
+Dicionarios
+
+Obs.: em algumas linguagens de programação, os dicionarios Python são conhecidos por mapas.
+
+Dicionarios são coleções do tipo chave/valor.
+
+Dicionarios são representados por {}.
+
+print(type({}))
+
+OBS: Sobre dicionarios
+    - Chave e valor são separados por dois pontos 'chave:valor'
+    - Tanto chave quanto valor podem ser de qualquer tipo de dados
+
+
+"""
+
+paises = {'br': 'Brasil', 'eua': 'Estados Unidos', 'py': 'Paraguai'}
+print(paises)
+print(type(paises))
