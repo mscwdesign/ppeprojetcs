a66d55b5a1f6308e4631f418abc78659ada2bb45
Mateus <mscwdesign@gmail.com>
2020-07-23 17:59:20 -0300

    None Python

---
+++ tipo_none.py
@@ -0,0 +1,25 @@
+"""
+
+tipo de dado none em python representa o tipo sem tipo ou poderia ser conhecido como tipo vazio
+porém falar que e tipo sem tipo e mais apropriado
+
+OBS o tipo none é sempre especificado com a primeira letra Maiuscula
+Certo: None
+Errado: none
+
+Quando utilizamos?
+
+- Podemos utilizar None quando queremos criar uma variavel e inicializa-la com um tipo sem tipo antes
+de recebermos um valor final.
+
+OBS O tipo none em python é sempre considerado como false
+"""
+
+numeros = None
+
+print(numeros)
+print(type(numeros))
+
+numeros = 1.44, 1.34, 5.67
+print(numeros)
+print(type(numeros))
