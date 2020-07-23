d1a989bbd4e94c2ed14b8d5eb2482964f3a367cd
Mateus <mscwdesign@gmail.com>
2020-07-23 09:36:58 -0300

    Condições Python

---
+++ condicionais_if_else_elif.py
@@ -0,0 +1,14 @@
+"""
+Estruturas Condicionais
+if, else, elif
+"""
+
+idade = 18
+
+if idade < 18:
+    print('Menor de Idade')
+elif idade == 18:
+    print('Tem 18 anos.')
+else:
+    print('Maior de Idade')
+
