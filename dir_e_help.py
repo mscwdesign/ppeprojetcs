b3128e3e631e8a7f465ea16f92218a4641bf9acd
Mateus <mscwdesign@gmail.com>
2020-07-23 09:38:26 -0300

    Dir e Help Python

---
+++ dir_e_help.py
@@ -0,0 +1,15 @@
+"""
+utilitarios python para te auxiliar na programação
+
+DIR -> Apresenta todos os atributos/propriedades e funçõe/metodos disponiveis para determinado tipo de dado ou variavel.
+
+dir(tipo de dado/variavel)
+
+Help -> Apresenta a documentação/como utilizar os atributos/propriedades e funções/metodos
+disponiveis para determinado tipo de dado ou variavel
+
+help(tipo de dado/variavel.propriedade)num.real()
+
+"""
+
+print(dir('geek'))
