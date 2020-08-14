d0c758dd3d560593e5653a2eb92108a9bd4d0074
Mateus <mscwdesign@gmail.com>
2020-08-14 17:51:32 -0300

    Tratativas de erro Entrada de Usuarios

---
+++ try_except_else_finally.py
@@ -0,0 +1,19 @@
+"""
+Try, Except, Else, Finally
+
+Dica de quando tratar o código
+
+Toda entrada deve ser tratada
+
+
+# Else exetuada somente se não ocorrer o erro
+
+try:
+    num = int(input('Informe um numero:'))
+except ValueError:
+    print('Valor Incorreto')
+else:
+    print(f'Você digitou {num}')
+"""
+
+
